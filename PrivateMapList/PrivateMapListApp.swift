99667a9b8d36f9efc5609afbe71d6b49f0c58b83
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-04-04 07:28:55 -0500

    init

---
+++ PrivateMapList/PrivateMapListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateMapListApp.swift
+//  PrivateMapList
+//
+//  Created by Christopher Peralta on 4/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PrivateMapListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
